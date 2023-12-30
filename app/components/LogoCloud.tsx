bbb281c8db0bdcb2467ca5ed3bd6dfcd4e322dfe
antonio <aloncar.mail@gmail.com>
2023-12-30 18:54:52 +0100

    add new logo

--- app/components/LogoCloud.tsx
+++ app/components/LogoCloud.tsx
@@ -1,10 +1,10 @@
 export default function LogoCloud() {
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <h2 className="font-semibold tracking-tight text-[#FF4F01] text-2xl mb-2 text-center">
+      <h2 className="font-semibold tracking-tight text-[#FF4F01] text-2xl mb-2 md:mb-5 text-center">
         trusted by
       </h2>
-      <div className="mx-auto max-w-lg items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none grid grid-cols-2 sm:grid-cols-4">
+      <div className="mx-auto max-w-lg items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-none grid grid-cols-2 md:grid-cols-5">
         <img
           className="max-h-12 w-full object-contain col-span-1"
           src="/paldesk.svg"
@@ -27,9 +27,16 @@ export default function LogoCloud() {
           height={48}
         />
         <img
-          className="max-h-12 w-full object-contain col-span-1"
+          className="max-h-12 w-full object-contain col-span-1 grayscale"
           src="/droppa.svg"
           alt="Droppa"
+          width={158}
+          height={48}
+        />
+        <img
+          className="max-h-12 w-full object-contain col-span-1 grayscale"
+          src="/phishar.svg"
+          alt="PhishAR"
           width={158}
           height={48}
         />
