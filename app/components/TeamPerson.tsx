95eda735c488d46e39858f276012446c1685a543
antonio <aloncar.mail@gmail.com>
2024-02-02 18:35:48 +0100

    add border on hover

--- app/components/TeamPerson.tsx
+++ app/components/TeamPerson.tsx
@@ -14,7 +14,12 @@ interface TeamPersonProps {
 
 export default function TeamPerson({ person }: TeamPersonProps) {
   return (
-    <Link to={person.linkedinUrl || "#"} target="_blank" rel="noreferrer">
+    <Link
+      to={person.linkedinUrl || "#"}
+      target="_blank"
+      rel="noreferrer"
+      className="transition duration-500 ease-in-out translate-y-1 py-1 px-4 border-transparent hover:border-[#FF4F01] border"
+    >
       <div className="flex items-center gap-x-6">
         <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
         <div>
